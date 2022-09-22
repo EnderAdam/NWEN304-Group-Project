0d187ce1d753598b1cd0886f4252dcdc4ebeca6a
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-09-22 17:18:12 +1200

    Add Mongoose and connect when the server starts up

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://dbUser:c43&B6HD6^oT2L^@nwen304project.utzxtry.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 server = http.createServer(app);
 server.listen(3000);
