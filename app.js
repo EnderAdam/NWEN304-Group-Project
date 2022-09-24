2cf2aea68361d5bc083c7746292b7f855548893c
Tarik Kurnaz <kurnaztari@ecs.vuw.ac.nz>
2022-09-24 18:00:43 +1200

    Added a functional /login route to login. Removed unused dependencies from package.json

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require("path");
 const http = require("http");
+const session = require('express-session');
+const passport = require('passport');
 
 const indexRouter = require('./routes/indexRouter');
 const apiRouter = require('./routes/apiRouter');
 const productRouter = require('./routes/productRouter');
-
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const Account = require('./models/account');
 
 const app = express();
 
@@ -15,10 +15,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+    secret: 'secretKey',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -27,6 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 app.use('/products', productRouter);
+
+passport.use(Account.createStrategy());
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use(function (req, res) {
     res.status(404);
@@ -45,27 +50,6 @@ app.use(function (req, res) {
 
     res.type('txt').send('Not found');
 });
-
-// passport config
-const Account = require('./models/account');
-// passport.use(new LocalStrategy(Account.authenticate()));
-// passport.use(new LocalStrategy(Account.authenticate(), function (username, password, done) {
-//     Account.findOne({ username: username }, function (err, user) {
-//         if (err) { return done(err); }
-//         if (!user) {
-//             return done(null, false, { message: 'Incorrect username.' });
-//         }
-//         if (!user.validPassword(password)) {
-//             return done(null, false, { message: 'Incorrect password.' });
-//         }
-//         return done(null, user);
-//     }).catch(err => done(err));
-// }), function (req, res) {
-//     res.redirect('/');
-// });
-passport.use(Account.createStrategy());
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,26 +1,63 @@
 const express = require('express');
 const router = express.Router();
 const {index, gorillaJacob} = require('../controllers/indexController');
+const connectEnsureLogin = require('connect-ensure-login');
 const passport = require("passport");
 const Account = require("../models/account");
 
+// GET Routes
 router.get('/', index);
-router.get('/login', gorillaJacob);
-router.post('/login', passport.authenticate('local'), function (req, res) {
-    res.redirect('/');
-});
-router.get('/register', function (req, res) {
-    res.render('register', {});
-});
-router.post('/register', function (req, res) {
-    Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
-        if (err) {
-            return res.render('register', {account: account});
-        }
 
-        passport.authenticate('local', {failureRedirect: '/'}, function (req, res,){
-            res.redirect('/');
-        });
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+router.get('/secret', connectEnsureLogin.ensureLoggedIn(), (req, res) =>
+    res.render('secret', { title: 'Secret Page' })
+);
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
     });
 });
+
+//TODO: Delete this route
+router.get('/ping', function (req, res) {
+    res.status(200).send("pong!");
+});
+
+// POST Routes
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        failureRedirect: '/login',
+        successRedirect: '/secret',
+    }),
+    (req, res) => {
+        console.log(req.user);
+    }
+);
+
+// router.post('/login', passport.authenticate('local'), function (req, res) {
+//     res.redirect('/');
+// });
+
+// router.get('/register', function (req, res) {
+//     res.render('register', {});
+// });
+//
+// router.post('/register', function (req, res) {
+//     Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
+//         if (err) {
+//             return res.render('register', {account: account});
+//         }
+//
+//         passport.authenticate('local', {failureRedirect: '/'}, function (req, res,){
+//             res.redirect('/');
+//         });
+//     });
+// });
+
 module.exports = router;
