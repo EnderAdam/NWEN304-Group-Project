1ea80e9121b62ebe8b4123ee1291efab1cc07534
Tarik Kurnaz <kurnaztari@ecs.vuw.ac.nz>
2022-09-24 23:28:32 +1200

    Added two buttons for logging in and registering in main screen. Changed MongoDB address Added error text for login and register pages.

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(function (req, res) {
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://dbUser:c43&B6HD6^oT2L^@nwen304project.utzxtry.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://dbUser:c43&B6HD6^oT2L^@nwen304project.utzxtry.mongodb.net/database?retryWrites=true&w=majority";
 
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
