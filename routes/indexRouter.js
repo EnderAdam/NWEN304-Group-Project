eb20a0a206c6573c0f0ae502116f99a716bbb3af
Tarik Kurnaz <kurnaztari@ecs.vuw.ac.nz>
2022-09-24 22:29:35 +1200

    Added the register functionality that leads to /secret now. Removed the unused index.js file

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const {index, gorillaJacob} = require('../controllers/indexController');
+const {index, loginGet, logoutGet, secret, registerGet, loginPost, registerPost} = require('../controllers/indexController');
 const connectEnsureLogin = require('connect-ensure-login');
-const passport = require("passport");
-const Account = require("../models/account");
 
 // GET Routes
 router.get('/', index);
 
-router.get('/login', (req, res) => {
-    res.render('login', { title: 'Login' });
-});
+router.get('/login', loginGet);
 
-router.get('/secret', connectEnsureLogin.ensureLoggedIn(), (req, res) =>
-    res.render('secret', { title: 'Secret Page' })
-);
+router.get('/secret', connectEnsureLogin.ensureLoggedIn(), secret);
 
-router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-    });
-});
+router.get('/logout', logoutGet);
+
+router.get('/register', registerGet);
 
 //TODO: Delete this route
 router.get('/ping', function (req, res) {
@@ -29,35 +20,8 @@ router.get('/ping', function (req, res) {
 });
 
 // POST Routes
-router.post(
-    '/login',
-    passport.authenticate('local', {
-        failureRedirect: '/login',
-        successRedirect: '/secret',
-    }),
-    (req, res) => {
-        console.log(req.user);
-    }
-);
+router.post('/login', loginPost);
 
-// router.post('/login', passport.authenticate('local'), function (req, res) {
-//     res.redirect('/');
-// });
-
-// router.get('/register', function (req, res) {
-//     res.render('register', {});
-// });
-//
-// router.post('/register', function (req, res) {
-//     Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
-//         if (err) {
-//             return res.render('register', {account: account});
-//         }
-//
-//         passport.authenticate('local', {failureRedirect: '/'}, function (req, res,){
-//             res.redirect('/');
-//         });
-//     });
-// });
+router.post('/register', registerPost);
 
 module.exports = router;
