a8541af06b05610d91085624722480ae36d123e6
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-10-17 18:19:05 +1300

    Add authentication checks for all admin routes for products

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {index, newProduct, create, show, edit, update, destroy} = require('../controllers/productController');
+const {checkAuthenticated} = require("../controllers/indexController");
 
 router.get('/', index);
-router.get('/new', newProduct);
-router.post('/create', create);
+router.get('/new', checkAuthenticated, newProduct);
+router.post('/create', checkAuthenticated, create);
 router.get('/:id', show);
-router.get('/:id/edit', edit);
-router.post('/:id/update', update);
-router.get('/:id/delete', destroy);
+router.get('/:id/edit', checkAuthenticated, edit);
+router.post('/:id/update', checkAuthenticated, update);
+router.get('/:id/delete', checkAuthenticated, destroy);
 
 module.exports = router;
