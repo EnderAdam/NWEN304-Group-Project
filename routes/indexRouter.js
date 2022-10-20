f082dd036b11806e61efc3b35e73e8e90ecaadb5
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-10-20 17:52:38 +1300

    Remove Secret page and cleanup Google Authentication routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,39 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const {
     index,
     loginGet,
     logoutGet,
-    secret,
     registerGet,
     loginPost,
     registerPost,
-    checkNotAuthenticated, isAdmin
+    checkNotAuthenticated,
+    isAdmin,
+    googlePage,
+    googleCallback
 } = require('../controllers/indexController');
 // GET Routes
 router.get('/', index);
 
 router.get('/login', checkNotAuthenticated, loginGet);
 
-router.get('/secret', isAdmin, secret);
-
 router.get('/logout', logoutGet);
 
 router.get('/register', checkNotAuthenticated, registerGet);
 
-//TODO: Move the following functions to indexController.js
-router.get('/oauth2/redirect/google/page',
-    passport.authenticate('google', {scope: ['profile', 'email']})
-);
+router.get('/oauth2/redirect/google/page', googlePage);
 
-router.get('/oauth2/redirect/google',
-    passport.authenticate('google', {failureRedirect: '/login'}),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/secret');
-    }
-);
+router.get('/oauth2/redirect/google', googleCallback);
 
 // POST Routes
 router.post('/login', loginPost);
