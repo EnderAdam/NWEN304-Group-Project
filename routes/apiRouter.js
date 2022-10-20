4ff2c4f3b9aa1e8f32aefb454549e0c4661c1cfc
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-10-20 16:08:37 +1300

    Add GET All products to API

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {create, show, update, destroy, login, authenticateToken} = require('../controllers/apiController');
+const {index, create, show, update, destroy, login, authenticateToken} = require('../controllers/apiController');
 
 router.post('/login', login);
 router.get('/products/:id', show);
+router.get('/products', index);
 router.post('/products/create',  authenticateToken, create);
 router.put('/products/:id',  authenticateToken, update);
 router.delete('/products/:id',   authenticateToken, destroy);
