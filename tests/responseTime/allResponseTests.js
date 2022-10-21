8dd47d2121ee9c80d536183030a8ec1e9ca8f35d
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-10-21 18:12:03 +1300

    Update main response time testing script to run all new load tests

--- tests/responseTime/allResponseTests.js
+++ tests/responseTime/allResponseTests.js
@@ -6,11 +6,11 @@ const server = require('../../app.js');
 
 // Require all the individual Test files
 const login = require('./POST Login.js');
-const getAllProducts = require('./GET All Products.js');
-const getProducts = require('./GET Product.js');
-const postProduct = require('./POST Product.js');
-const putProduct = require('./PUT Product.js');
-const deleteProduct = require('./DELETE Product.js');
+const [getAllProducts, getAllProductsLoad] = require('./GET All Products.js');
+const [getProducts, getProductsLoad] = require('./GET Product.js');
+const [postProduct, postProductLoad] = require('./POST Product.js');
+const [putProduct, putProductLoad] = require('./PUT Product.js');
+const [deleteProduct, deleteProductLoad]  = require('./DELETE Product.js');
 
 async function runTests() {
     // POST /login
@@ -18,23 +18,28 @@ async function runTests() {
 
     // GET /products
     console.log("GET api/products");
-    await getAllProducts(token);
+    await getAllProducts();
+    await getAllProductsLoad();
 
     // GET /products/:id
     console.log("GET api/products/:id");
     await getProducts();
+    await getProductsLoad();
 
     // POST /products/create
     console.log("POST api/products/create");
     let ids = await postProduct(token);
+    let idsLoad = await postProductLoad(token);
 
     // PUT /products/:id
     console.log("PUT api/products/:id");
     await putProduct(token, ids[0], ids[1]);
+    await putProductLoad(token, idsLoad[0], idsLoad[1]);
 
     // DELETE /products/:id
     console.log("DELETE api/products/:id");
     await deleteProduct(token, ids[0], ids[1]);
+    await deleteProductLoad(token, idsLoad[0], idsLoad[1]);
 
     // Close the server and exit the process
     server.close();
