fc39f391f4625019b618dedf078147dbccaa37f3
Jacob Chapman <chapmajaco@ecs.vuw.ac.nz>
2022-10-21 16:34:44 +1300

    Add load testing response times for GET individual product

--- tests/responseTime/GET Product.js
+++ tests/responseTime/GET Product.js
@@ -34,4 +34,52 @@ async function getProducts() {
     console.log(`Remote Average: ${remoteAverage}ms`);
 }
 
-module.exports = getProducts;
+/**
+ * Load test the GET Products endpoint with 500 and 1000 concurrent requests
+ */
+async function getProductsLoad() {
+    const id = "632e9adc45ac4aaec91deb8e";
+    const remoteUrl = `https://nwen304theconnoisseurs.herokuapp.com/api/products/${id}`;
+
+    let responseTimes500 = [];
+    let responseTimes1000 = [];
+    let promises = [];
+
+
+    // 500 concurrent requests
+    for (let i = 0; i < 500; i++) {
+        let start = new Date().getTime();
+        promises.push(fetch(remoteUrl).then(() => {
+            let end = new Date().getTime();
+            responseTimes500.push(end - start);
+        }));
+    }
+    await Promise.all(promises);
+    promises = [];
+
+    // 1000 concurrent requests
+    for (let i = 0; i < 1000; i++) {
+        let start = new Date().getTime();
+        promises.push(fetch(remoteUrl).then(() => {
+            let end = new Date().getTime();
+            responseTimes1000.push(end - start);
+        }));
+    }
+    await Promise.all(promises);
+
+
+    // Average results
+    let total500;
+    let total1000;
+
+    total500 = responseTimes500.reduce((a, b) => a + b, 0);
+    total1000 = responseTimes1000.reduce((a, b) => a + b, 0);
+
+    let average500 = total500 / responseTimes500.length;
+    let average1000 = total1000 / responseTimes1000.length;
+
+    console.log(`500 Concurrent Requests Average: ${average500}ms`);
+    console.log(`1000 Concurrent Requests Average: ${average1000}ms`);
+}
+
+module.exports = [getProducts, getProductsLoad];
