064b74960bd5e0deac17a654cbfb2335f3f75e57
Tarik Kurnaz <kurnaztari@ecs.vuw.ac.nz>
2022-10-22 13:57:49 +1300

    Fixed order of tests and created an after for test

--- tests/authorizationTest/authentication.test.js
+++ tests/authorizationTest/authentication.test.js
@@ -1,48 +1,20 @@
 const User = require("../../models/account");
-const express = require("../../app");
+const app = require("../../app");
 const request = require("supertest");
 const chai = require("chai");
 const should = require("chai").should();
 
-describe("Authentication", () => {
-
-    const newUser = {"username": "-3test-new@techbrij.com", "password": "tesT!"};
+async function runTests() {
+    const newUser = {"username": "-5test-new@techbrij.com", "password": "tesT!"};
     const existingUser = {"username": "1test-new@techbrij.com", "password": "tesT!"};
 
-    it('Create a user', (done) => {
-        // const expectedResponse = [user]
-        request(express)
-            .post('/register')
-            .send(newUser)
-            .expect(302)
-            .end((err, res) => {
-                res.text.should.contain("Redirecting to /");
-                if (!err) {
-                    done();
-                    return;
-                }
-                done("Should not be here");
-            })
-    })
-
-    it('Login with a user', (done) => {
-        // const expectedResponse = [user]
-        request(express)
-            .post('/login')
-            .send(existingUser)
-            .expect(302)
-            .end((err, res) => {
-                res.text.should.contain("Redirecting to /");
-                if (!err) {
-                    done();
-                    return;
-                }
-                done("Should not have an error");
-            })
+    after(async () => {
+        // Close the server and exit the process
+        app.close();
     });
 
     it("Login with a non-existing user", (done) => {
-        request(express)
+        request(app)
             .post('/login')
             .send(newUser)
             .expect(200)
@@ -59,22 +31,59 @@ describe("Authentication", () => {
             })
     });
 
-    it("should not login with wrong password", (done) => {
-        const wrongPasswordUser = {"username": "1test-new@techbrij.com", "password": "tesT!!"};
-        request(express)
-            .post('/login')
+    it('Create a user', (done) => {
+        // const expectedResponse = [user]
+        request(app)
+            .post('/register')
             .send(newUser)
-            .expect(200)
+            .expect(302)
             .end((err, res) => {
-                console.log(err + " " + res);
+                res.text.should.contain("Redirecting to /");
                 if (!err) {
-                    done("Should not be here");
+                    done();
                     return;
                 }
-                done();
+                done("Should not be here");
+            })
+    })
+
+    it('Login with a user', (done) => {
+        // const expectedResponse = [user]
+        request(app)
+            .post('/login')
+            .send(existingUser)
+            .expect(302)
+            .end((err, res) => {
+                res.text.should.contain("Redirecting to /");
+                if (!err) {
+                    done();
+                    return;
+                }
+                done("Should not have an error");
             })
     });
-});
+
+
+
+    it("should not login with wrong password", (done) => {
+        const wrongPasswordUser = {"username": "1test-new@techbrij.com", "password": "tesT!!"};
+        request(app)
+            .post('/login')
+            .send(wrongPasswordUser)
+            .expect(200)
+            .end((err, res) => {
+                if (res.text.includes("Password or username is incorrect")) {
+                    if (!err) {
+                        done();
+                        return;
+                    }
+                    done("shouldn't have an error");
+                    return;
+                }
+                done("response text should include 'Password or username is incorrect'");
+            })
+    });
+}
 
 // describe("# Auth APIs", () => {
 //     const apiBase = process.env.API_BASE || '/api';
@@ -116,3 +125,8 @@ describe("Authentication", () => {
 //             });
 //     });
 // });
+
+// Node.JS weirdness required to run an async function from the top level
+(async () => {
+    await runTests();
+})();
